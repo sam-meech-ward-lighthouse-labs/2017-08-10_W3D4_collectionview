c9cdbc7ffd1012042cfdba0b9f861887ba850a9e
Sam Meech-Ward <sam@meech-ward.com>
2017-08-10 09:06:44 -0700

    Initial Commit

---
+++ Movies/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Movies
+//
+//  Created by Sam Meech-Ward on 2017-08-10.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
